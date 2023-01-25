909e6bf67e05f3d227e379fbf0d591d9bf7940c6
flazepe <flazepe@gmail.com>
2023-01-25 11:08:42 +0800

    feat: scrobbling

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,66 @@
+mod config;
+mod last_fm;
+mod nerinyan;
+mod osu;
+mod osu_scrobble;
+mod spotify;
+
 use async_recursion::async_recursion;
+use osu_scrobble::OsuScrobble;
 use std::time::Duration;
 use tokio::{main, time};
 
-mod config;
-mod last_fm;
-mod osu;
-mod spotify;
-
 #[async_recursion]
-async fn check_loop(scrobbler: rustfm_scrobble::Scrobbler, old_title: String) {
-    let mut title = old_title.to_string();
+async fn check_loop(scrobbler: &rustfm_scrobble::Scrobbler, mut osu_scrobble: Option<OsuScrobble>) {
+    let config = config::get_config();
 
-    match osu::get_osu_window_title() {
-        Some(new_title) => {
-            let (separated_title, separated_artist) = osu::separate_title_and_artist(&new_title);
+    match osu::get_osu_window_details() {
+        Some(window_details) => {
+            if osu_scrobble.is_none()
+                || &osu_scrobble.as_ref().unwrap().window_details.title != &window_details.title
+            {
+                if let Some(beatmapset) = nerinyan::get_beatmapset(&window_details).await {
+                    if beatmapset.length >= config.options.min_length_seconds {
+                        osu_scrobble = Some(OsuScrobble::new(&window_details, &beatmapset));
 
-            last_fm::set_now_playing(
-                &scrobbler,
-                &separated_title,
-                &separated_artist,
-                &separated_title,
-            );
+                        last_fm::set_now_playing(
+                            &scrobbler,
+                            if config.options.use_original_metadata {
+                                &beatmapset.title_unicode
+                            } else {
+                                &beatmapset.title
+                            },
+                            if config.options.use_original_metadata {
+                                &beatmapset.artist_unicode
+                            } else {
+                                &beatmapset.artist
+                            },
+                            if config.options.use_original_metadata {
+                                &beatmapset.title_unicode
+                            } else {
+                                &beatmapset.title
+                            },
+                        );
 
-            if new_title != old_title {
-                title = new_title.to_string();
-                println!("osu! is playing: {}", new_title);
-
-                /*
-                if let Some(track) = spotify::get_track(&new_title).await {
-                    last_fm::set_now_playing(&scrobbler, &track.title, &track.artist, &track.album);
-
-                    println!(
-                        "Set now playing to {} by {} on {} (length: {}s)!",
-                        &track.title,
-                        &track.artist,
-                        &track.album,
-                        &track.length / 1000
-                    );
+                        println!(
+                            "Now playing: {}!",
+                            osu_scrobble.as_ref().unwrap().window_details.raw_title
+                        );
+                    } else {
+                        osu_scrobble = None;
+                    }
+                } else {
+                    osu_scrobble = None;
+                    println!("Could not find {} in mirror!", window_details.raw_title);
                 }
-                */
             }
         }
         None => {
-            if old_title != "" {
-                title = String::from("");
-                println!("osu! is playing: -");
+            if let Some(osu_scrobble) = &osu_scrobble {
+                osu_scrobble.end(&scrobbler);
             }
+
+            osu_scrobble = None;
         }
     }
 
@@ -54,10 +69,11 @@ async fn check_loop(scrobbler: rustfm_scrobble::Scrobbler, old_title: String) {
     interval.tick().await;
     interval.tick().await;
 
-    check_loop(scrobbler, title).await
+    check_loop(scrobbler, osu_scrobble).await
 }
 
 #[main]
 async fn main() {
-    check_loop(last_fm::get_scrobbler(), String::from("")).await;
+    println!("Started loop!");
+    check_loop(&last_fm::get_scrobbler(), None).await;
 }
