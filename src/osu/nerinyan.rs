d7eeca9e2fcc7c15c35d702bffc9e4d768547a55
flazepe <flazepe@gmail.com>
2023-01-25 17:24:57 +0800

    fix: handle maps that aren't in database

--- src/osu/nerinyan.rs
+++ src/osu/nerinyan.rs
@@ -14,39 +14,39 @@ pub struct Beatmapset {
 }
 
 pub fn get_beatmapset(details: &OsuWindowDetails) -> Option<Beatmapset> {
-    let json: Vec<Value> = Client::new()
+    if let Ok(json) = Client::new()
         .get(
             "https://api.nerinyan.moe/search?q=".to_string()
                 + &encode((details.artist.to_owned() + " - " + &details.title).as_str()),
         )
         .send()
         .unwrap()
-        .json()
-        .unwrap();
+        .json::<Vec<Value>>()
+    {
+        for beatmapset in json {
+            if (beatmapset["artist"].as_str().unwrap() == details.artist.as_str()
+                || beatmapset["artist_unicode"].as_str().unwrap() == details.artist.as_str())
+                && (beatmapset["title"].as_str().unwrap() == details.title.as_str()
+                    || beatmapset["title_unicode"].as_str().unwrap() == details.title.as_str())
+            {
+                let mut target_beatmap: Option<Value> = None;
 
-    for beatmapset in json {
-        if (beatmapset["artist"].as_str().unwrap() == details.artist.as_str()
-            || beatmapset["artist_unicode"].as_str().unwrap() == details.artist.as_str())
-            && (beatmapset["title"].as_str().unwrap() == details.title.as_str()
-                || beatmapset["title_unicode"].as_str().unwrap() == details.title.as_str())
-        {
-            let mut target_beatmap: Option<Value> = None;
-
-            for beatmap in beatmapset["beatmaps"].as_array().unwrap() {
-                if beatmap["version"].as_str().unwrap() == details.beatmap.as_str() {
-                    target_beatmap = Some(beatmap.to_owned());
-                    break;
+                for beatmap in beatmapset["beatmaps"].as_array().unwrap() {
+                    if beatmap["version"].as_str().unwrap() == details.beatmap.as_str() {
+                        target_beatmap = Some(beatmap.to_owned());
+                        break;
+                    }
                 }
-            }
 
-            if target_beatmap.is_some() {
-                return Some(Beatmapset {
-                    artist: beatmapset["artist"].as_str().unwrap().to_owned(),
-                    artist_unicode: beatmapset["artist_unicode"].as_str().unwrap().to_owned(),
-                    title: beatmapset["title"].as_str().unwrap().to_owned(),
-                    title_unicode: beatmapset["title_unicode"].as_str().unwrap().to_owned(),
-                    length: target_beatmap.unwrap()["total_length"].as_u64().unwrap(),
-                });
+                if target_beatmap.is_some() {
+                    return Some(Beatmapset {
+                        artist: beatmapset["artist"].as_str().unwrap().to_owned(),
+                        artist_unicode: beatmapset["artist_unicode"].as_str().unwrap().to_owned(),
+                        title: beatmapset["title"].as_str().unwrap().to_owned(),
+                        title_unicode: beatmapset["title_unicode"].as_str().unwrap().to_owned(),
+                        length: target_beatmap.unwrap()["total_length"].as_u64().unwrap(),
+                    });
+                }
             }
         }
     }
