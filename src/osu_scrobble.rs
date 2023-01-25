232c09cbae8ff3283080de9c5da086e686bb76df
flazepe <flazepe@gmail.com>
2023-01-25 12:24:37 +0800

    feat: scrobble if past 4 minutes as per last.fm docs

--- src/osu_scrobble.rs
+++ src/osu_scrobble.rs
@@ -9,15 +9,17 @@ use std::time::{SystemTime, UNIX_EPOCH};
 pub struct OsuScrobble {
     pub window_details: OsuWindowDetails,
     pub beatmapset: Beatmapset,
-    end_timestamp: u64,
+    end_timestamps: Vec<u64>,
 }
 
 impl OsuScrobble {
     pub fn new(window_details: &OsuWindowDetails, beatmapset: &Beatmapset) -> Self {
+        let timestamp = OsuScrobble::get_current_timestamp();
+
         Self {
             window_details: window_details.to_owned(),
             beatmapset: beatmapset.to_owned(),
-            end_timestamp: OsuScrobble::get_current_timestamp() + (beatmapset.length / 2),
+            end_timestamps: vec![timestamp + (beatmapset.length / 2), timestamp + 240],
         }
     }
 
@@ -29,7 +31,14 @@ impl OsuScrobble {
     }
 
     pub fn end(&self, scrobbler: &Scrobbler) {
-        if OsuScrobble::get_current_timestamp() >= self.end_timestamp {
+        let timestamp = OsuScrobble::get_current_timestamp();
+
+        if self
+            .to_owned()
+            .end_timestamps
+            .into_iter()
+            .any(|end_timestamp| timestamp >= end_timestamp)
+        {
             println!("Scrobbled!");
 
             let config = get_config();
