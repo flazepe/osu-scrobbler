d2e19280891500735ef314ac5e08357de9094f4f
jupiter <syl@waifu.club>
2023-06-27 17:35:56 +1000

    refactor: improve error handling and code organization in listenbrainz
    - separate token validation and object initialization logic into distinct functions
    - replace unwrap and panic with proper error handling using `result` and the `?` operator
    - remove unnecessary string formatting in API requests for better performance
    - provide more informative error messages for token validation and network errors

--- src/scrobbler/listenbrainz.rs
+++ src/scrobbler/listenbrainz.rs
@@ -17,53 +17,61 @@ struct ListenBrainzToken {
 }
 
 impl ListenBrainzScrobbler {
-    pub fn new(user_token: String) -> Self {
+    pub fn new(user_token: String) -> Result<Self, String> {
         let client = Client::new();
-
-        match client
-            .get(format!("{API_BASE_URL}/validate-token"))
-            .header("authorization", format!("Token {user_token}"))
+        let response = client
+            .get(format!("{}/validate-token", API_BASE_URL))
+            .header("authorization", format!("Token {}", user_token))
             .send()
-            .unwrap()
-            .json::<ListenBrainzToken>()
-        {
-            Ok(token) => {
-                println!("{} Successfully authenticated with username {}.", "[ListenBrainz]".bright_green(), token.user_name.bright_blue())
-            },
-            Err(_) => panic!("{} Invalid user token provided.", "[ListenBrainz]".bright_red()),
-        };
+            .map_err(|error| format!("Error validating user token: {}", error))?;
 
-        Self { client, user_token }
+        if response.status().is_success() {
+            let token = response
+                .json::<ListenBrainzToken>()
+                .map_err(|error| format!("Error parsing token response: {}", error))?;
+            println!(
+                "{} Successfully authenticated with username {}.",
+                "[ListenBrainz]".bright_green(),
+                token.user_name.bright_blue()
+            );
+            Ok(Self { client, user_token })
+        } else {
+            Err(format!("Invalid user token provided."))
+        }
     }
 
     pub fn scrobble(&self, title: &str, artist: &str, total_length: u32) -> Result<(), String> {
-        match self
-            .client
-            .post(format!("{API_BASE_URL}/submit-listens"))
-            .header("authorization", format!("Token {}", self.user_token))
-            .json(&json!({
-                "listen_type": "single",
-                "payload": [{
-                    "listened_at": SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs(),
-                    "track_metadata": {
-                        "artist_name": artist,
-                        "track_name": title,
-                        "additional_info": {
-                            "media_player": "osu!",
-                            "submission_client": "osu-scrobbler (github.com/flazepe/osu-scrobbler)",
-                            "submission_client_version": env!("CARGO_PKG_VERSION"),
-                            "duration_ms": total_length * 1000,
-                        },
+        let listen_data = json!({
+            "listen_type": "single",
+            "payload": [{
+                "listened_at": SystemTime::now()
+                    .duration_since(UNIX_EPOCH)
+                    .map_err(|error| format!("Error getting current time: {}", error))?
+                    .as_secs(),
+                "track_metadata": {
+                    "artist_name": artist,
+                    "track_name": title,
+                    "additional_info": {
+                        "media_player": "osu!",
+                        "submission_client": "osu-scrobbler (github.com/flazepe/osu-scrobbler)",
+                        "submission_client_version": env!("CARGO_PKG_VERSION"),
+                        "duration_ms": total_length * 1000,
                     },
-                }],
-            }))
+                },
+            }],
+        });
+
+        let response = self
+            .client
+            .post(format!("{}/submit-listens", API_BASE_URL))
+            .header("authorization", format!("Token {}", self.user_token))
+            .json(&listen_data)
             .send()
-        {
-            Ok(response) => match response.status() {
-                StatusCode::OK => Ok(()),
-                status_code => Err(format!("Received status code {status_code}.")),
-            },
-            Err(error) => Err(error.to_string()),
+            .map_err(|error| format!("Error sending scrobble request: {}", error))?;
+
+        match response.status() {
+            StatusCode::OK => Ok(()),
+            status_code => Err(format!("Received status code {}.", status_code)),
         }
     }
 }
