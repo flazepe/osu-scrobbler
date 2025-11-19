fbac5297676d659468b86948836b9a97b3c995ba
flazepe <51487578+flazepe@users.noreply.github.com>
2025-11-19 14:20:03 +0800

    feat: prioritize blacklist first while validating scrobble

--- src/scrobbler/mod.rs
+++ src/scrobbler/mod.rs
@@ -154,39 +154,6 @@ impl Scrobbler {
     }
 
     fn validate_scrobble(&mut self, score: &Score) -> Result<()> {
-        if score.beatmap.total_length < self.config.min_beatmap_length_secs {
-            bail!(
-                "Beatmap's total length ({}) is less than the configured minimum length ({}).",
-                format!("{}s", score.beatmap.total_length).bright_blue(),
-                format!("{}s", self.config.min_beatmap_length_secs).bright_blue(),
-            );
-        }
-
-        if !score.passed {
-            let started_at = score.started_at.as_ref().and_then(|started_at| DateTime::parse_from_rfc3339(started_at).ok());
-            let ended_at = DateTime::parse_from_rfc3339(&score.ended_at).ok();
-            let delta =
-                ended_at.and_then(|ended_at| started_at.map(|started_at| (ended_at - started_at).as_seconds_f64())).unwrap_or_default();
-
-            if delta > 0. {
-                let rate = score
-                    .mods
-                    .iter()
-                    .find(|score_mod| score_mod.acronym == "DT" || score_mod.acronym == "NC")
-                    .and_then(|dt_or_nc_mod| dt_or_nc_mod.settings.as_ref().map(|settings| settings.speed_change.unwrap_or(1.5)))
-                    .unwrap_or(1.);
-                let hit_length = score.beatmap.hit_length as f64 / rate;
-
-                // A valid scrobble should be half of the beatmap's hit length or 4 minutes, whichever occurs earlier
-                // This might go through if the user paused, took a long break, and continued (just to fail some time after)
-                let is_valid_scrobble = delta >= hit_length / 2. || delta >= 60. * 4.;
-
-                if !is_valid_scrobble {
-                    bail!("Play's progress is less than half of the drain time.");
-                }
-            }
-        }
-
         let (artist_romanized, artist_original) = (score.beatmapset.artist.to_lowercase(), score.beatmapset.artist_unicode.to_lowercase());
 
         if self.config.blacklist.artists.equals.contains(&artist_romanized)
@@ -233,6 +200,39 @@ impl Scrobbler {
 
         if let Some(word) = self.config.blacklist.difficulties.contains_word.iter().find(|word| boundary_match(&difficulty, word)) {
             bail!("Beatmapset difficulty contains a blacklisted word ({}).", word.bright_red());
+        }
+
+        if score.beatmap.total_length < self.config.min_beatmap_length_secs {
+            bail!(
+                "Beatmap's total length ({}) is less than the configured minimum length ({}).",
+                format!("{}s", score.beatmap.total_length).bright_blue(),
+                format!("{}s", self.config.min_beatmap_length_secs).bright_blue(),
+            );
+        }
+
+        if !score.passed {
+            let started_at = score.started_at.as_ref().and_then(|started_at| DateTime::parse_from_rfc3339(started_at).ok());
+            let ended_at = DateTime::parse_from_rfc3339(&score.ended_at).ok();
+            let delta =
+                ended_at.and_then(|ended_at| started_at.map(|started_at| (ended_at - started_at).as_seconds_f64())).unwrap_or_default();
+
+            if delta > 0. {
+                let rate = score
+                    .mods
+                    .iter()
+                    .find(|score_mod| score_mod.acronym == "DT" || score_mod.acronym == "NC")
+                    .and_then(|dt_or_nc_mod| dt_or_nc_mod.settings.as_ref().map(|settings| settings.speed_change.unwrap_or(1.5)))
+                    .unwrap_or(1.);
+                let hit_length = score.beatmap.hit_length as f64 / rate;
+
+                // A valid scrobble should be half of the beatmap's hit length or 4 minutes, whichever occurs earlier
+                // This might go through if the user paused, took a long break, and continued (just to fail some time after)
+                let is_valid_scrobble = delta >= hit_length / 2. || delta >= 60. * 4.;
+
+                if !is_valid_scrobble {
+                    bail!("Play's progress is less than half of the drain time.");
+                }
+            }
         }
 
         Ok(())
